af63530bd31f4be8ac35a2bcb7bd733fd3cf31da
Hakkan Parvez <hakkanparvez@gmail.com>
2025-08-24 19:20:37 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <div class="flex-g

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,11 +36,14 @@ export default function RootLayout({
     <html lang="en" className="h-full dark">
       <body
         className={cn(
-          'relative h-full antialiased font-code aurora-bg',
+          'relative h-full antialiased font-code stars-bg',
           fontBody.variable,
           fontCode.variable
         )}
       >
+        <div id="stars1"></div>
+        <div id="stars2"></div>
+        <div id="stars3"></div>
         <main className="relative flex flex-col min-h-screen">
           <Header />
           <div className="flex-grow flex-1">{children}</div>
