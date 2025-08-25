33d2a4ce26b7014d947184dd0a82de7a1c0c23ce
Hakkan Parvez <hakkanparvez@gmail.com>
2025-08-25 11:06:59 +0000

    you just read the code and made no changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -52,7 +52,7 @@ export default function RootLayout({
         <div id="stars3"></div>
         <Header />
         <main className="relative flex flex-col min-h-screen">
-          <div className="flex-grow flex-1 pt-24">{children}</div>
+          <div className="flex-grow flex-1 pt-32">{children}</div>
           <footer className="py-6 md:px-8 md:py-0">
             <div className="container flex flex-col items-center justify-between gap-4 md:h-24 md:flex-row">
               <p className="text-balance text-center text-sm leading-loose text-muted-foreground md:text-left">
